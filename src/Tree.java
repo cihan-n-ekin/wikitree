95ec8063bc70d185bb4ff7cd50c0f5241a4bc3a5
Cihan Bosnalı <cihanbosna@hotmail.com>
2018-06-21 15:19:45 +0300

    Tree add species

--- src/Tree.java
+++ src/Tree.java
@@ -1,3 +1,5 @@
+import com.google.gson.JsonElement;
+
 import java.util.ArrayList;
 
 public class Tree extends Node {
@@ -24,7 +26,77 @@ public class Tree extends Node {
     }
 
     public void addSpecies(Species newSpecies) {
-        //TODO: add addSpecies() method
+
+        // Kingdom
+        JsonElement kingdom = newSpecies.taxon.kingdom.get("scientificName");
+        ArrayList<Node> kingdoms = getLevel(1);
+        boolean newKingdomRequired = true;
+        for (Node king:kingdoms) {
+            if (kingdom.getAsString() == king.id) {
+                newKingdomRequired = false;
+            }
+
+        }
+
+        int firstLevel = 1;
+
+
+
+        switch(firstLevel){
+            case 1:
+                String kingname = newSpecies.taxon.kingdom.get("scientificName").getAsString();
+                Node king = new Node(kingname);
+            case 2:
+                String phyname = newSpecies.taxon.phylum.get("scientificName").getAsString();
+                Node phy = new Node(phyname);
+            case 3:
+                String subphyname = newSpecies.taxon.subphylum.get("scientificName").getAsString();
+                Node subphy = new Node(subphyname);
+            case 4:
+                String ordname = newSpecies.taxon.order.get("scientificName").getAsString();
+                Node ord = new Node(ordname);
+            case 5:
+                String subordname = newSpecies.taxon.suborder.get("scientificName").getAsString();
+                Node subord = new Node(subordname);
+            case 6:
+                String clasname = newSpecies.taxon.clss.get("scientificName").getAsString();
+                Node clas = new Node(clasname);
+            case 7:
+
+        }
+
+
+
+                String specname = newSpecies.taxon.species.get("scientificName").getAsString();
+        String genusname = newSpecies.taxon.genus.get("scientificName").getAsString();
+        String famname = newSpecies.taxon.family.get("scientificName").getAsString();
+
+
+
+
+
+        Node spec = new Node(specname);
+        Node genus = new Node(genusname);
+        Node fam  = new Node(famname);
+
+
+
+
+
+
+
+        genus.addChild(spec);
+        fam.addChild(genus);
+        clas.addChild(fam);
+        subord.addChild(clas);
+        ord.addChild(subord);
+        subphy.addChild(ord);
+        phy.addChild(subphy);
+        king.addChild(phy);
+        this.addChild(king);
+
+
+
     }
 
 
